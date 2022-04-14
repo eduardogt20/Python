62ee121cba92cb0f0f34996d02ac110c41b7538a
Bairon Garita <Bairongaba@gmail.com>
2022-04-14 10:01:51 -0600

    More python

--- constitution/2.Intermediate/7.Herencia/4.Super.py
+++ constitution/2.Intermediate/7.Herencia/4.Super.py
@@ -1,11 +1,13 @@
-#Super
+# Super
 
 class Padre:
     def __init__(self):
         self.padre = "Bairon"
         print(f"El Padre se llama{self.padre}")
+
     def caminar(self):
         print("Esta caminando")
+
 
 class Hijo(Padre):
     def __init__(self):
@@ -13,10 +15,11 @@ class Hijo(Padre):
         self.hijo = "Matias"
         print(f"El Hijo se llama{self.hijo}")
         super().caminar
+
     def Saludar(self):
         super().caminar()
         print("Saludar")
 
+
 hijo = Hijo()
 hijo.Saludar()
-
