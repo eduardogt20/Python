c82813532bede42f44b993f1d3269087e3aed44e
Bairon Garita <Bairongaba@gmail.com>
2022-05-20 16:35:21 -0600

    List

---
+++ constitution/2.Intermediate/3.Comprehensions/list_Comprenhension.py
@@ -0,0 +1,24 @@
+#Definicion
+#newList = [ expression(element) for element in oldList if condition ] 
+
+# Use Loops
+# Empty list
+List = []
+ 
+# Traditional approach of iterating
+for character in 'Geeks 4 Geeks!':
+    List.append(character)
+ 
+# Display list
+print(List)
+
+# Use List 
+# Empty list
+List = []
+ 
+# Traditional approach of iterating
+for character in 'Geeks 4 Geeks!':
+    List.append(character)
+ 
+# Display list
+print(List)
